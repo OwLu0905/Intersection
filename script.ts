05cb9bfd0ddbb2801a0182f5be954c9aa966e78a
OwLu0905 <owlu0905@gmail.com>
2024-01-25 23:45:56 +0800

    add scoll section

--- script.ts
+++ script.ts
@@ -5,17 +5,16 @@ const navEle = document.querySelector("nav");
 function stickyNav(entries: IntersectionObserverEntry[]) {
   const entry = entries[0];
 
-  console.log(entry);
   if (!entry.isIntersecting) {
     navEle?.classList.add("fixed");
     navEle?.classList.add("top-0");
-    navEle?.classList.add("bg-white/60");
-    navEle?.classList.remove("bg-gray-200");
+    navEle?.classList.add("bg-white/80");
+    navEle?.classList.remove("bg-white");
   } else {
     navEle?.classList.remove("fixed");
     navEle?.classList.remove("top-0");
-    navEle?.classList.add("bg-gray-200");
-    navEle?.classList.remove("bg-white/60");
+    navEle?.classList.add("bg-white");
+    navEle?.classList.remove("bg-white/80");
   }
 }
 
@@ -31,4 +30,51 @@ const headerObserver = new IntersectionObserver(stickyNav, navObserverOption);
 
 if (headerEle) {
   headerObserver.observe(headerEle);
+}
+
+// NOTE: section animate ////////////////////////////////
+
+const sectionEles = document.querySelectorAll("section");
+
+sectionEles.forEach((section) => section.classList.add("transition-all"));
+sectionEles.forEach((section) => section.classList.add("duration-1000"));
+sectionEles.forEach((section) => section.classList.add("translate-y-96"));
+sectionEles.forEach((section) => section.classList.add("opacity-0"));
+
+function sectionAnimate(
+  entries: IntersectionObserverEntry[],
+  observer: IntersectionObserver
+) {
+  entries.forEach((i) => {
+    if (i.isIntersecting) {
+      requestAnimationFrame(() => {
+        i.target.classList.remove("opacity-0");
+        i.target.classList.remove("translate-y-96");
+        i.target.classList.add("opacity-100");
+      });
+    }
+  });
+
+  const entry = entries[0];
+
+  if (entry.isIntersecting) {
+    entry.target.classList.remove("opacity-0");
+    entry.target.classList.remove("translate-y-96");
+    entry.target.classList.add("opacity-100");
+    observer.unobserve(entry.target);
+  }
+}
+
+const sectionObserverOption: IntersectionObserverInit = {
+  root: null,
+  threshold: 0.5,
+};
+
+const sectionObservers = new IntersectionObserver(
+  sectionAnimate,
+  navObserverOption
+);
+
+if (sectionEles) {
+  sectionEles.forEach((sectionEle) => sectionObservers.observe(sectionEle));
 }
